b7f214ba2f0088053cd2494284d3c27aabee1d15
Alan Gutierrez <alan@blogometer.com>
2009-11-24 13:53:36 -0600

    Changed brace style of Dirtyable.

--- src/main/java/com/goodworkalan/region/Dirtyable.java
+++ src/main/java/com/goodworkalan/region/Dirtyable.java
@@ -1,13 +1,12 @@
 package com.goodworkalan.region;
 
 /**
- * Used to mark regions of a buffer as dirty and in need of writing to disk.
- * The dirtyable interface parallels a byte buffer.
- *  
+ * Used to mark regions of a buffer as dirty and in need of writing to disk. The
+ * dirtyable interface parallels a byte buffer.
+ * 
  * @author Alan Gutierrez
  */
-public interface Dirtyable
-{
+public interface Dirtyable {
     /**
      * Get the length of the buffer.
      * 
