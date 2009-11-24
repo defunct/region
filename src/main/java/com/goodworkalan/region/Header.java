726e3eb051fecea0c67541de4fd4775c7c1c0d7a
Alan Gutierrez <alan@blogometer.com>
2009-11-24 15:06:31 -0600

    Javadoc for Header and HeaderBuilder.

--- src/main/java/com/goodworkalan/region/Header.java
+++ src/main/java/com/goodworkalan/region/Header.java
@@ -5,44 +5,83 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.locks.Lock;
 
-// TODO Document.
-public class Header<K> extends BasicWritable
-{
-    // TODO Document.
+/**
+ * An array of regions in a file representing header fields. The header is
+ * written by writing to
+ * 
+ * @author Alan Gutierrez
+ * 
+ * @param <K>
+ *            The header key type.
+ */
+public class Header<K> extends BasicWritable {
+    /**
+     * The map of keys to a offset/length pairs. The lists are two element lists
+     * containing the field offset and field length.
+     */
     private final Map<K, List<Integer>> offsets;
 
-    // TODO Document.
-    public Header(long position, Map<K, List<Integer>> offsets, ByteBuffer byteBuffer, Lock lock)
-    {
+    /**
+     * Create a header at the given file position that maps the given map of
+     * keys to offsets. The given byte buffer is used to buffer the header
+     * fields before they are written do disk. The given lock is used to lock
+     * the byte buffer for reading and writer.
+     * 
+     * @param position
+     *            The position of the region on disk.
+     * @param offset
+     *            The map of keys to a offset/length pairs. The lists are two
+     *            element lists containing the field offset and field length.
+     * @param byteBuffer
+     *            The byte buffer of header content.
+     * @param lock
+     *            A lock used to lock the header for reading and writing.
+     * @param cleanable
+     *            The cleanable interface used to record dirtyed bytes in the
+     *            byte buffer.
+     */
+    Header(long position, Map<K, List<Integer>> offsets, ByteBuffer byteBuffer, Lock lock) {
         super(position, byteBuffer, lock, new DirtyByteMap(byteBuffer.capacity()));
         this.offsets = offsets;
     }
-    
-    // TODO Document.
-    public Region get(K key)
-    {
+
+    /**
+     * Get the header field region associated with the given key. The field
+     * region contain a byte buffer slice of the header focused on the header
+     * field region.
+     * <p>
+     * The byte buffer slice will start at the position of the header filed in
+     * the header byte buffer. The capacity of the byte buffer slice will be the
+     * header field length.
+     * <p>
+     * The field region will contain a lock, shared by all field regions
+     * returned by the header, that locks the entire header for reading and
+     * writing.
+     * 
+     * @param key
+     *            The header field key
+     * @return A region for the header field.
+     */
+    public Region get(K key) {
         List<Integer> offset = offsets.get(key);
         Dirtyable subDirtyable = new SubCleanable(cleanable, offset.get(0), offset.get(1));
         getLock().lock();
-        try
-        {
+        try {
             ByteBuffer byteBuffer = getByteBuffer();
-            
+
             int position = byteBuffer.position();
             int limit = byteBuffer.limit();
-            
+
             byteBuffer.position(offset.get(0));
             byteBuffer.limit(offset.get(0) + offset.get(1));
-            
+
             ByteBuffer subByteBuffer = byteBuffer.slice();
-            
+
             byteBuffer.position(position);
             byteBuffer.limit(limit);
-            
+
             return new BasicRegion(position + offset.get(0), subByteBuffer, getLock(), subDirtyable);
-        }
-        finally
-        {
+        } finally {
             getLock().unlock();
         }
     }
--- src/main/java/com/goodworkalan/region/HeaderBuilder.java
+++ src/main/java/com/goodworkalan/region/HeaderBuilder.java
@@ -8,24 +8,27 @@ import java.util.Map;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
- * Build a file header by defining keyed fields by specifying their key
- * and their length.
+ * A builder for a header that defines the header by mapping a header field key
+ * to a header field length. The header files will be arrayed on file one after
+ * another in the order in which they are specified.
  * 
  * @author Alan Gutierrez
- *
- * @param <K> Type type of key.
+ * 
+ * @param <K>
+ *            The header key type.
  */
-public class HeaderBuilder<K>
-{
+public class HeaderBuilder<K> {
     /** The offset of the next field. */
     private int offset;
-    
-    /** A map of keys to a offset/length pairs. */
+
+    /**
+     * The map of keys to a offset/length pairs. The lists are two element lists
+     * containing the field offset and field length.
+     */
     private final Map<K, List<Integer>> offsets;
-    
+
     /** Create a new header builder. */
-    public HeaderBuilder()
-    {
+    public HeaderBuilder() {
         this.offsets = new HashMap<K, List<Integer>>();
     }
 
@@ -38,8 +41,7 @@ public class HeaderBuilder<K>
      * @param length
      *            The field length.
      */
-    public void addField(K key, int length)
-    {
+    public void addField(K key, int length) {
         List<Integer> mapping = new ArrayList<Integer>(2);
         mapping.add(offset);
         mapping.add(length);
@@ -55,8 +57,7 @@ public class HeaderBuilder<K>
      *            The file position.
      * @return A new file header.
      */
-    public Header<K> newHeader(long position)
-    {
+    public Header<K> newHeader(long position) {
         ByteBuffer byteBuffer = ByteBuffer.allocate(offset);
         return new Header<K>(position, offsets, byteBuffer, new ReentrantLock());
     }
