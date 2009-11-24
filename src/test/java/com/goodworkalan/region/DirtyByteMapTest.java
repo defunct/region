201dcbe531e860391beed067616b183d0d486947
Alan Gutierrez <alan@blogometer.com>
2009-11-24 15:15:29 -0600

    Updated brace style of DirtyByteMapTest.

--- src/test/java/com/goodworkalan/region/DirtyByteMapTest.java
+++ src/test/java/com/goodworkalan/region/DirtyByteMapTest.java
@@ -8,37 +8,35 @@ import org.testng.annotations.Test;
 
 import com.goodworkalan.region.DirtyByteMap;
 
-public class DirtyByteMapTest
-{
+public class DirtyByteMapTest {
     @Test
-    public void dirty() throws IOException
-    {
+    public void dirty() throws IOException {
         DirtyByteMap dirtyByteMap = new DirtyByteMap(64);
-        
+
         dirtyByteMap.dirty(0, 64);
-        
+
         dirtyByteMap.dirty(3, 3);
 
         dirtyByteMap.dirty(7, 3);
-        
+
         dirtyByteMap.dirty(3, 3);
         dirtyByteMap.dirty(6, 4);
-        
+
         dirtyByteMap.dirty(6, 4);
         dirtyByteMap.dirty(3, 3);
         assertEquals(1, dirtyByteMap.dirtied.size());
-        
+
         dirtyByteMap.clean();
 
         // Two invalid regions.
         dirtyByteMap.dirty(3, 3);
         dirtyByteMap.dirty(7, 3);
         assertEquals(2, dirtyByteMap.dirtied.size());
-        
+
         // First region extended by one.
         dirtyByteMap.dirty(2, 4);
         assertEquals(2, dirtyByteMap.dirtied.size());
-        
+
         // First region replace by larger region and merged into second region.
         dirtyByteMap.dirty(2, 5);
         assertEquals(1, dirtyByteMap.dirtied.size());
@@ -46,13 +44,13 @@ public class DirtyByteMapTest
         // Invalidating an already invalid region.
         dirtyByteMap.dirty(3, 3);
         assertEquals(1, dirtyByteMap.dirtied.size());
-        
+
         // Extending a region.
         dirtyByteMap.dirty(8, 3);
         assertEquals(1, dirtyByteMap.dirtied.size());
-        
+
         dirtyByteMap.clean();
-        
+
         // Swallowing entire regions.
         dirtyByteMap.dirty(3, 3);
         dirtyByteMap.dirty(7, 3);
@@ -63,18 +61,15 @@ public class DirtyByteMapTest
         assertEquals(1, dirtyByteMap.dirtied.size());
     }
 
-    @Test(expectedExceptions=java.lang.IllegalStateException.class)
-    public void regionalLowerRange()
-    {
+    @Test(expectedExceptions = java.lang.IllegalStateException.class)
+    public void regionalLowerRange() {
         DirtyByteMap dirtyByteMap = new DirtyByteMap(64);
         dirtyByteMap.dirty(-1, 10);
     }
 
-    @Test(expectedExceptions=java.lang.IllegalStateException.class)
-    public void regionalUpperRange()
-    {
+    @Test(expectedExceptions = java.lang.IllegalStateException.class)
+    public void regionalUpperRange() {
         DirtyByteMap dirtyByteMap = new DirtyByteMap(64);
         dirtyByteMap.dirty(0, 65);
     }
 }
-
