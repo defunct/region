9d099d3e28785f51ac9dd6980b35bacf4f0cc742
Alan Gutierrez <alan@blogometer.com>
2009-11-24 15:14:58 -0600

    Updated brace style of SubCleanable.

--- src/main/java/com/goodworkalan/region/SubCleanable.java
+++ src/main/java/com/goodworkalan/region/SubCleanable.java
@@ -1,18 +1,17 @@
 package com.goodworkalan.region;
 
 /**
- * A cleanable that reports on a buffer slice of a region buffer. 
- *
+ * A cleanable that reports on a buffer slice of a region buffer.
+ * 
  * @author Alan Gutierrez
  */
-public class SubCleanable implements Cleanable
-{
+public class SubCleanable implements Cleanable {
     /** The offset into the parent cleanable. */
     private final int offset;
-    
+
     /** The length of the the buffer slice. */
     private final int length;
-    
+
     /** The parent. */
     private final Cleanable cleanable;
 
@@ -27,28 +26,25 @@ public class SubCleanable implements Cleanable
      * @param length
      *            The length of the the buffer slice.
      */
-    public SubCleanable(Cleanable dirtyable, int offset, int length)
-    {
+    public SubCleanable(Cleanable dirtyable, int offset, int length) {
         this.offset = offset;
         this.length = length;
         this.cleanable = dirtyable;
     }
-    
+
     /**
      * Get the length of the buffer slice.
      * 
      * @return The length of the buffer slice.
      */
-    public int getLength()
-    {
+    public int getLength() {
         return length;
     }
 
     /**
      * Mark the entire buffer slice as dirty.
      */
-    public void dirty()
-    {
+    public void dirty() {
         dirty(0, length);
     }
 
@@ -61,10 +57,8 @@ public class SubCleanable implements Cleanable
      * @param length
      *            The length of the dirty region.
      */
-    public void dirty(int offset, int length)
-    {
-        if (length > getLength())
-        {
+    public void dirty(int offset, int length) {
+        if (length > getLength()) {
             throw new IndexOutOfBoundsException();
         }
         cleanable.dirty(this.offset + offset, length);
@@ -79,20 +73,17 @@ public class SubCleanable implements Cleanable
      * @param length
      *            The length of the dirty region.
      */
-    public void clean(int offset, int length)
-    {
-        if (length > getLength())
-        {
+    public void clean(int offset, int length) {
+        if (length > getLength()) {
             throw new IndexOutOfBoundsException();
         }
         cleanable.clean(this.offset + offset, length);
     }
-    
+
     /**
      * Mark the entire byte buffer slice as clean.
      */
-    public void clean()
-    {
+    public void clean() {
         clean(0, length);
     }
 }
