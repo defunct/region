2071a24927297fd72d82d732f389d6c91c27efae
Alan Gutierrez <alan@blogometer.com>
2010-05-05 03:16:19 -0500

    Updated Mix build for new project language.

--- src/mix/java/com/goodworkalan/region/mix/RegionProject.java
+++ src/mix/java/com/goodworkalan/region/mix/RegionProject.java
@@ -1,6 +1,5 @@
 package com.goodworkalan.region.mix;
 
-import com.goodworkalan.go.go.Artifact;
 import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
@@ -10,11 +9,11 @@ public class RegionProject extends ProjectModule {
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
-                .produces(new Artifact("com.goodworkalan/region/0.1"))
+                .produces("com.github.bigeasy.region/region/0.1")
                 .test()
                     .depends()
-                        .artifact(new Artifact("org.mockito/mockito-core/1.6"))
-                        .artifact(new Artifact("org.testng/testng/5.10"))
+                        .include("org.mockito/mockito-core/1.6")
+                        .include("org.testng/testng-jdk15/5.10")
                         .end()
                     .end()
                 .end()
