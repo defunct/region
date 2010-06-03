b6338bc675b1c16fea9cad97e13e4735bd5ba2ff
Alan Gutierrez <alan@blogometer.com>
2010-06-03 01:37:00 -0500

    Mix ProjectModule is now an interface.

--- src/mix/java/com/goodworkalan/region/mix/RegionProject.java
+++ src/mix/java/com/goodworkalan/region/mix/RegionProject.java
@@ -4,17 +4,25 @@ import com.goodworkalan.mix.ProjectModule;
 import com.goodworkalan.mix.builder.Builder;
 import com.goodworkalan.mix.builder.JavaProject;
 
-public class RegionProject extends ProjectModule {
-    @Override
+/**
+ * Builds the project definition for Region.
+ *
+ * @author Alan Gutierrez
+ */
+public class RegionProject implements ProjectModule {
+    /**
+     * Build the project definition for Region.
+     *
+     * @param builder
+     *          The project builder.
+     */
     public void build(Builder builder) {
         builder
             .cookbook(JavaProject.class)
                 .produces("com.github.bigeasy.region/region/0.1")
-                .test()
-                    .depends()
-                        .include("org.mockito/mockito-core/1.6")
-                        .include("org.testng/testng-jdk15/5.10")
-                        .end()
+                .depends()
+                    .development("org.mockito/mockito-core/1.6")
+                    .development("org.testng/testng-jdk15/5.10")
                     .end()
                 .end()
             .end();
